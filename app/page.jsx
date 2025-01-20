5e54970c7e804b625fb9bc66272b12d4d1f9412b
shreyansh <shrey@fpslounge.com>
2025-01-20 19:08:35 +0530

    build fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,10 @@
 'use client'
 
-import Duckhunt from '@/components/canvas/Duckhunt'
-import { PhysicsWorld } from '@/components/canvas/PhysicsWorld'
-import PeerCall from '@/components/PeerCall'
-import VideoCall from '@/components/RtcComponent'
-import SocketComponent from '@/components/SocketComponent'
-import { CameraShake, PerspectiveCamera, PositionalAudio } from '@react-three/drei'
-import { useFrame, useThree } from '@react-three/fiber'
+import { PositionalAudio } from '@react-three/drei'
 import dynamic from 'next/dynamic'
 import { Suspense, useRef, useState } from 'react'
 
-const Logo = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
-const Dog = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
 const Duck = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Duck), { ssr: false })
-const BoxUi = dynamic(() => import('@/components/canvas/BoxUi').then((mod) => mod.Box), { ssr: false })
 
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
   ssr: false,
@@ -31,12 +22,6 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
   ),
 })
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
-let arr = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, -2, -3, -4, -5, -6]
-const BoxLoop = ({ y }) => {
-  return arr.map((ele, index) => (
-    <BoxUi key={Math.random() * ele} scale={0.02} position={[ele, Math.random() * 3 - 1, 0]} />
-  ))
-}
 
 export default function Page() {
   const audioRef = useRef()
@@ -53,60 +38,30 @@ export default function Page() {
   const [mousePos, setMousePos] = useState([0, 0])
 
   const handleMouseMove = (event) => {
-    // Normalize mouse position in viewport coordinates
     setMousePos([(event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1])
   }
 
   return (
     <>
-      {/* <Duckhunt /> */}
-      {/* asdasdasd */}
       <div
         ref={meshRef}
         onMouseMove={handleMouseMove}
         onClick={() => gunAudio()}
         className='grid size-full grid-cols-1 items-center justify-items-center overflow-y-scroll '
       >
-        {/* <SocketComponent /> */}
-        {/* jumbo */}
-        {/* <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
-        </div> */}
-        {/* <VideoCall /> */}
-        <PeerCall />
         <div className=' size-full '>
           <View className=' grid size-full grid-cols-3 bg-[url("/icons/field.jpg")]  bg-cover '>
             <Suspense fallback={null}>
-              {/* <Duck scale={0.02} pos={mousePos} />
+              <Duck scale={0.02} pos={mousePos} />
               <PositionalAudio
                 ref={audioRef}
                 url='/shot.mp3' // Path to your audio file
                 distance={1}
-              /> */}
-
+              />
               <Common />
             </Suspense>
           </View>
         </div>
-        {/* <div className=' size-full  bg-red-800 '>
-          <View orbit className=' h-full w-screen '>
-            {' '}
-            <Suspense fallback={null}>
-              <BoxUi scale={0.02} position={[0, 0, 0]} />
-              <Common />
-            </Suspense>
-          </View>
-        </div>
-        <div className=' size-full  bg-green-800 '>
-          <View orbit className=' h-full w-screen '>
-            <Suspense fallback={null}>
-              <Logo route='/blob' scale={0.2} position={[0, 0, 0]} />
-              <Common />
-            </Suspense>
-          </View>
-        </div>{' '} */}
       </div>
     </>
   )
